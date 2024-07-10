f85af64e129d265debb2255cc356d3291752c399
thilomueller <thilo.r.mueller@gmail.com>
2024-07-11 00:10:44 +0200

    fixed number of arguments in construct_QETU_circ() function call in test case

--- tests/test_qetu.py
+++ tests/test_qetu.py
@@ -18,7 +18,7 @@ class TestQETU(unittest.TestCase):
         mat_step_func = scipy.io.loadmat('phase_angles/step_function_02_d' + str(degree) + '.mat')['phi_proc']
         step_function_qsp_angles = list(itertools.chain.from_iterable(mat_step_func))
         phi_vec = convert_Zrot_to_Xrot(step_function_qsp_angles)
-        QETU_circ = construct_QETU_circ(u, t, degree, trotter_steps, phi_vec)
+        QETU_circ = construct_QETU_circ(u, t, trotter_steps, phi_vec)
         QETU_circ_WMI = transpile_QETU_to_WMI(QETU_circ)
         initial_state = Statevector.from_label("0-+0++++0")
         final_state = qetu_sim(QETU_circ_WMI, initial_state)
