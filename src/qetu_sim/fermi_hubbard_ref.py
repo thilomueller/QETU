78636a350586e631e2e4a024f33a726ba5deb76c
thilomueller <thilo.r.mueller@gmail.com>
2024-07-26 23:00:04 +0200

    refactored calculation of reference Hamiltonian calculation and added option to change qubit ordering to WMI

--- src/qetu_sim/fermi_hubbard_ref.py
+++ src/qetu_sim/fermi_hubbard_ref.py
@@ -1,81 +1,164 @@
 import scipy as sp
 import numpy as np
+import functools as ft
 
-I = sp.sparse.csr_array(np.array([[1, 0], [0, 1]]))
-X = sp.sparse.csr_array(np.array([[0, 1], [1, 0]]))
-Y = sp.sparse.csr_array(np.array([[0, -1j], [1j, 0]]))
-Z = sp.sparse.csr_array(np.array([[1, 0], [0, -1]]))
+Id = np.array([[1, 0], [0, 1]])
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1j], [1j, 0]])
+Z = np.array([[1, 0], [0, -1]])
 
-op_1 = sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # X_1 X_3
-op_2 = sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Y_1 Y_3
-op_3 = sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # X_2 X_4
-op_4 = sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Y_2 Y_4
-op_5 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(X, sp.sparse.kron(I, I))))))) # X_5 X_6
-op_6 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(Y, sp.sparse.kron(I, I))))))) # Y_5 Y_6
-op_7 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, X))))))) # X_7 X_8
-op_8 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, Y))))))) # Y_7 Y_8
-op_9 = sp.sparse.kron(X, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(Z, sp.sparse.kron(I, I))))))) # X_1 X_2 Z_5 Z_6
-op_10 = sp.sparse.kron(Y, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(Z, sp.sparse.kron(I, I))))))) # Y_1 Y_2 Z_5 Z_6
-op_11 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, Z))))))) # X_3 X_4 Z_7 Z_8
-op_12 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, Z))))))) # Y_3 Y_4 Z_7 Z_8
-op_13 = sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(X, I))))))) # Z_1 Z_3 X_5 X_7
-op_14 = sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(Y, I))))))) # Z_1 Z_3 Y_5 Y_7
-op_15 = sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, X))))))) # Z_2 Z_4 X_6 X_8
-op_16 = sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, Y))))))) # Z_2 Z_4 Y_6 Y_8
+def H_1(onsite_pairs, include_aux):
+    """
+    This function creates the onsite Hamiltonian for given qubit positions.
 
-op_17 = sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # X_1 X_5
-op_18 = sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Y_1 Y_5
-op_19 = sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_1 Z_5
-op_20 = sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_1
-op_21 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_5
+    Args:
+        onsite_pairs: list of tuple
+            A list of qubit pair positions indicating where the onsite terms should be inserted
+        include_aux: boolean
+            Indicates if the auxiliary qubit should be included
+    
+    Returns:
+        H_1: numpy array
+            Hamiltonian of the onsite terms.
+    """
+    if include_aux:
+        num_qubits = 9
+    else:
+        num_qubits = 8
+    H_1 = np.zeros((2**num_qubits, 2**num_qubits), dtype="complex128")
+    for qubit_pair in onsite_pairs:
+        q1 = qubit_pair[0]
+        q2 = qubit_pair[1]
+        ZZ_list = [Z if x==q1 else Z if x==q2 else Id for x in range(num_qubits)]
+        ZZ = ft.reduce(np.kron, ZZ_list)
+        H_1 += ZZ
+    return H_1
 
-op_22 = sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, I))))))) # X_2 X_6
-op_23 = sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, I))))))) # Y_2 Y_6
-op_24 = sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, I))))))) # Z_2 Z_6
-op_25 = sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_2
-op_26 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, I))))))) # Z_6
+def H_2(hopping_pairs_1, include_aux):
+    """
+    This function creates the Hamiltonian of horizontal spin-down and vertical spin-up hopping interaction terms
+    for given qubit positions.
+    
+    Args:
+        hopping_pairs_1: list of tuple
+            A list of qubit pair positions indicating where the hopping terms should be inserted
+        include_aux: boolean
+            Indicates if the auxiliary qubit should be included
+    
+    Returns:
+        H_1: numpy array
+            Hamiltonian of the hopping terms.
+    """
+    if include_aux:
+        num_qubits = 9
+    else:
+        num_qubits = 8
+    H_2 = np.zeros((2**num_qubits, 2**num_qubits), dtype="complex128")
+    for qubit_pair in hopping_pairs_1:
+        q1 = qubit_pair[0]
+        q2 = qubit_pair[1]
+        XX_list = [X if x==q1 else X if x==q2 else Id for x in range(num_qubits)]
+        XX = ft.reduce(np.kron, XX_list)
+        YY_list = [Y if x==q1 else Y if x==q2 else Id for x in range(num_qubits)]
+        YY = ft.reduce(np.kron, YY_list)
+        H_2 += (XX + YY)
+    return H_2
 
-op_27 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, I))))))) # X_3 X_7
-op_28 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, I))))))) # Y_3 Y_7
-op_29 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, I))))))) # Z_3 Z_7
-op_30 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_3
-op_31 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, I))))))) # Z_7
+def H_3(hopping_pairs_2, include_aux):
+    """
+    This function creates the Hamiltonian of vertical spin-down and horizontal spin-up hopping interaction terms
+    for given qubit positions.
+    
+    Args:
+        hopping_pairs_1: list of tuple
+            A list of qubit pair positions indicating where the hopping terms should be inserted
+        include_aux: boolean
+            Indicates if the auxiliary qubit should be included
+    
+    Returns:
+        H_1: numpy array
+            Hamiltonian of the hopping terms.
+    """
+    if include_aux:
+        num_qubits = 9
+    else:
+        num_qubits = 8
+    H_3 = np.zeros((2**num_qubits, 2**num_qubits), dtype="complex128")
+    for qubit_pair in hopping_pairs_2:
+        q1 = qubit_pair[0]
+        q2 = qubit_pair[1]
+        q3 = qubit_pair[2]
+        q4 = qubit_pair[3]
+        XX_list = [X if x==q1 else X if x==q2 else Id for x in range(num_qubits)]
+        XX_list = [Z if x==q3 else Z if x==q4 else XX_list[x] for x in range(num_qubits)]
+        XX = ft.reduce(np.kron, XX_list)
+        YY_list = [Y if x==q1 else Y if x==q2 else Id for x in range(num_qubits)]
+        YY_list = [Z if x==q3 else Z if x==q4 else YY_list[x] for x in range(num_qubits)]
+        YY = ft.reduce(np.kron, YY_list)
+        H_3 += (XX + YY)
+    return H_3
 
-op_32 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(X, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, X))))))) # X_4 X_8
-op_33 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Y, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, Y))))))) # Y_4 Y_8
-op_34 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, Z))))))) # Z_4 Z_8
-op_35 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(Z, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, I))))))) # Z_4
-op_36 = sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, sp.sparse.kron(I, Z))))))) # Z_8
-
-def ref_fh_hamiltonian(u=2, t=1):
+def ref_fh_hamiltonian(u=2, t=1, WMI_qubit_layout=False, include_aux=False):
     """
     Creates a reference Hamiltonian of the 2x2 Fermi Hubbard model.
 
     Args:
-        u - Coulomb repulsion energy
-        t - kinetic hopping term
-        delta_t - time difference
+        u: float
+            Coulomb repulsion energy
+        t: float
+            kinetic hopping term
+        WMI_qubit_layout: boolean
+            Indicates if the qubit layout should follow the optimal mapping on the WMI grid-like architecture.
+            If false, the qubit ordering will just follow the textbook convention
+        include_aux: boolean
+            Indicates if the auxiliary qubit should be included
 
     Returns:
         A numpy array of the reference Hamiltonian.
     """
-    H = 0.25*u*(op_19 + op_24 + op_29 + op_34) -2*t*(op_1 + op_2 + op_3 + op_4 + op_5 + op_6 + op_7 + op_8
-                                                                   + op_9 + op_10 + op_11 + op_12 + op_13 + op_14 + op_15 + op_16)
-    return H.toarray()
+    if WMI_qubit_layout:
+        onsite_pairs = [(2,5), (7,8), (1,0), (6,3)]
+        hopping_pairs_1 = [(2,1), (7,6), (5,8), (0,3)]
+        hopping_pairs_2 = [(2,7,5,8), (1,6,0,3), (5,0,2,1), (8,3,7,6)]
+        assert include_aux == True, "When using the WMI qubit layout, the auxiliary qubit must be included"
+    else:
+        onsite_pairs = [(0,4), (1,5), (2,6), (3,7)]
+        hopping_pairs_1 = [(0,2), (1,3), (4,5), (6,7)]
+        hopping_pairs_2 = [(0,1,4,5), (2,3,6,7), (4,6,0,2), (5,7,1,3)]
+    H1 = H_1(onsite_pairs, include_aux)
+    H2 = H_2(hopping_pairs_1, include_aux)
+    H3 = H_3(hopping_pairs_2, include_aux)
+    H = 0.25*u*H1 - 2*t*(H2 + H3)
+    return H
 
-def ref_fh_op(u=2, t=1, delta_t=1):
+def ref_fh_op(u=2, t=1, delta_t=1, WMI_qubit_layout=False):
     """
     Creates a reference time evolution operator of the 2x2 Fermi Hubbard model.
 
     Args:
-        u - Coulomb repulsion energy
-        t - kinetic hopping term
-        delta_t - time difference
+        u: float
+            Coulomb repulsion energy
+        t: float
+            kinetic hopping term
+        delta_t: float
+            time difference
+        WMI_qubit_layout: boolean
+            Indicates if the qubit layout should follow the optimal mapping on the WMI grid-like architecture.
+            If false, the qubit ordering will just follow the textbook convention
 
     Returns:
         A numpy array of the reference time evolution operator.
     """
-    U = sp.sparse.linalg.expm(-1j*0.25*u*delta_t*(op_19 + op_24 + op_29 + op_34)
-                              -1j*-2*t*delta_t*(op_1 + op_2 + op_3 + op_4 + op_5 + op_6 + op_7 + op_8
-                                                + op_9 + op_10 + op_11 + op_12 + op_13 + op_14 + op_15 + op_16))
-    return U.toarray()
+    if WMI_qubit_layout:
+        onsite_pairs = [(2,5), (7,8), (1,0), (6,3)]
+        hopping_pairs_1 = [(2,1), (7,6), (5,8), (0,3)]
+        hopping_pairs_2 = [(2,7,5,8), (1,6,0,3), (5,0,2,1), (8,3,7,6)]
+    else:
+        onsite_pairs = [(0,4), (1,5), (2,6), (3,7)]
+        hopping_pairs_1 = [(0,2), (1,3), (4,5), (6,7)]
+        hopping_pairs_2 = [(0,1,4,5), (2,3,6,7), (4,6,0,2), (5,7,1,3)]
+    H1 = H_1(onsite_pairs, False)
+    H2 = H_2(hopping_pairs_1, False)
+    H3 = H_3(hopping_pairs_2, False)
+    U = sp.linalg.expm(-1j*0.25*u*H1 -1j*-2*t*(H2 + H3))
+    return U
